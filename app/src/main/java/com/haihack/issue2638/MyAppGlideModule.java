0dba1ff66e17ec66d8418f79c61e8f34d53670eb
haihack <chuhoanghai0901@gmail.com>
2017-11-25 10:54:10 +0700

    First commit

---
+++ app/src/main/java/com/haihack/issue2638/MyAppGlideModule.java
@@ -0,0 +1,13 @@
+package com.haihack.issue2638;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by haihack on 11/23/2017.
+ */
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+
+}
